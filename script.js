cf826b5681a01610f43189a6aa6d4b53030fe486
Vinay Kumar Goud <144577819+vinay1723@users.noreply.github.com>
2026-02-21 22:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let button = document.getElementById('sum_btn');
+let num1 = document.getElementById('n1');
+let num2 = document.getElementById('n2');
+let res = document.getElementById('sum');
+button.addEventListener('click',(e) => {
+	e.preventDefault();
+	let sum = Number(num1.value) + Number(num2.value);
+	res.textContent = `${sum}`;
+})
